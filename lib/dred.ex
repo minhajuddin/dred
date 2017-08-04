d86943d9c08aa0b88f1df2281c2fc6134ca714e2
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2017-08-04 17:20:14 +0530

    added getter and putter

--- lib/dred.ex
+++ lib/dred.ex
@@ -1,18 +1,39 @@
 defmodule Dred do
-  @moduledoc """
-  Documentation for Dred.
-  """
 
-  @doc """
-  Hello world.
+  require Record
+  Record.defrecord Search, [id: nil, data: nil]
 
-  ## Examples
+  # initializes the db
+  def init do
+    set_db_dir()
+    :mnesia.create_schema(db_nodes()) |> IO.inspect(label: "SCHEMA")
+    :ok = :mnesia.start
+    :mnesia.create_table(Search, [
+      attributes: [:id, :data],
+      disc_copies:  db_nodes(),
+      type: :set, # :ordered_set, :bag
+    ]) |> IO.inspect(label: "TABLE")
+  end
 
-      iex> Dred.hello
-      :world
+  def put(search_id, data) do
+    :mnesia.transaction(fn ->
+      :mnesia.write({Search, search_id, data})
+    end)
+  end
 
-  """
-  def hello do
-    :world
+  def get(search_id) do
+    :mnesia.transaction(fn ->
+      :mnesia.read(Search, search_id)
+    end)
+  end
+
+  # TODO: make this dynamic so that it can connect to other nodes
+  defp db_nodes do
+    Application.get_env(:dred, :nodes) || [node()]
+  end
+
+  defp set_db_dir(dir \\ nil) do
+    dir = dir || Path.expand("./db/")
+    Application.put_env(:mnesia, :dir, dir |> to_charlist)
   end
 end
